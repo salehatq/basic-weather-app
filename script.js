3dd2b9a993d0b02269df3c3c480895f574909b4e
Saleh <justtsalih@gmail.com>
2026-06-29 19:50:46 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, Welcome to the Weather App!")
+// API for realtime weather data -- http://api.weatherapi.com/v1/current.json?key=b793ad3d7d4444a791493152262406&q=Lucknow&aqi=no
+
+let target= 'Lucknow';
+
+const fetchResults = async (targetLocation) => {
+    let url = "http://api.weatherapi.com/v1/current.json?key=b793ad3d7d4444a791493152262406&q=${targetLocation}&aqi=no"
+
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data)
+    let locationName = data.location.name;
+    let time = data.location.localtime;
+    let temp = data.current.temp_c;
+}
+
+fetchResults(target)
